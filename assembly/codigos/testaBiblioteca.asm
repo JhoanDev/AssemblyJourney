7b30470a3bcf7dbdd5231544ec7d68827c1c43a8
JhoanDev <jhoandev.log@gmail.com>
2024-03-01 09:36:07 -0300

    feat: biblioteca

---
+++ assembly/codigos/testaBiblioteca.asm
@@ -0,0 +1,30 @@
+%include 'bibliotecaE.inc'
+
+section .text
+    global _start
+
+_start:
+    lea rsi, [BUFFER]; loade effective address
+    add rsi, 0x09
+    mov byte[esi], LN
+
+    dec esi
+    mov dl, 0x13 ; -> C 0x33 -> c
+    add dl, '0'
+    mov [esi], dl
+
+    dec esi
+    mov dl, 0x12 ; -> B 0x32 -> b
+    add dl, '0'
+    mov [esi], dl
+
+    dec esi
+    mov dl, 0x11 ; -> A 0x31 -> a
+    add dl, '0'
+    mov [esi], dl
+
+    call saidaResultado
+
+    mov eax, SYS_EXIT
+    mov ebx, RET_EXIT
+    int SYS_CALL
